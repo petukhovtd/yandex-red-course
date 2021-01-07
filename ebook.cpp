74abc9004a630bab2885aa6e6e7c51defba4b7b4
Kseniya <mikhailovaxenia3@gmail.com>
2021-01-07 13:21:44 +0700

    2021-01-07

--- ebook.cpp
+++ ebook.cpp
@@ -7,71 +7,63 @@
 
 using namespace std;
 
-class ReadingManager {
+class ReadingManager
+{
 public:
     ReadingManager()
-            : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
-              sorted_users_(),
-              user_positions_(MAX_USER_COUNT_ + 1, -1) {}
+            : users_()
+            , pages_()
+    {}
 
-    void Read(int user_id, int page_count) {
-        if (user_page_counts_[user_id] == 0) {
-            AddUser(user_id);
+    void Read(int user_id, int page_count)
+    {
+        if( users_.count( user_id ) == 1 )
+        {
+            int lastPage = users_[ user_id ];
+            pages_[ lastPage ].userCount--;
         }
-        user_page_counts_[user_id] = page_count;
-        int& position = user_positions_[user_id];
-        while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
-            SwapUsers(position, position - 1);
-        }
+
+        users_[ user_id ] = page_count;
+        pages_[ page_count ].userCount++;
+        PagesRateUpdate();
     }
 
-    double Cheer(int user_id) const {
-        if (user_page_counts_[user_id] == 0) {
+    double Cheer(int user_id) const
+    {
+        auto it = users_.find( user_id );
+        if( it == users_.end() )
+        {
             return 0;
         }
-        const int user_count = GetUserCount();
-        if (user_count == 1) {
-            return 1;
-        }
-        const int page_count = user_page_counts_[user_id];
-        int position = user_positions_[user_id];
-        while (position < user_count &&
-               user_page_counts_[sorted_users_[position]] == page_count) {
-            ++position;
-        }
-        if (position == user_count) {
-            return 0;
-        }
-        // По умолчанию деление целочисленное, поэтому
-        // нужно привести числитель к типу double.
-        // Простой способ сделать это — умножить его на 1.0.
-        return (user_count - position) * 1.0 / (user_count - 1);
+        return pages_.at( users_.at( user_id ) ).rate;
     }
 
 private:
-    // Статическое поле не принадлежит какому-то конкретному
-    // объекту класса. По сути это глобальная переменная,
-    // в данном случае константная.
-    // Будь она публичной, к ней можно было бы обратиться снаружи
-    // следующим образом: ReadingManager::MAX_USER_COUNT.
-    static const int MAX_USER_COUNT_ = 100'000;
+    struct PageRate
+    {
+        size_t userCount;
+        double rate;
+    };
 
-    vector<int> user_page_counts_;
-    vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
-    vector<int> user_positions_; // позиции в векторе sorted_users_
+    std::map< int, int > users_;
+    std::map< int, PageRate > pages_;
 
-    int GetUserCount() const {
-        return sorted_users_.size();
-    }
-    void AddUser(int user_id) {
-        sorted_users_.push_back(user_id);
-        user_positions_[user_id] = sorted_users_.size() - 1;
-    }
-    void SwapUsers(int lhs_position, int rhs_position) {
-        const int lhs_id = sorted_users_[lhs_position];
-        const int rhs_id = sorted_users_[rhs_position];
-        swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-        swap(user_positions_[lhs_id], user_positions_[rhs_id]);
+    void PagesRateUpdate()
+    {
+        size_t usersSize = users_.size();
+
+        if( usersSize == 1 )
+        {
+            pages_.begin()->second.rate = 1;
+            return;
+        }
+
+        size_t count = 0;
+        for( auto& page: pages_ )
+        {
+            page.second.rate = count * 1.0 / ( usersSize - 1 );
+            count += page.second.userCount;
+        }
     }
 };
 
